1a7d7c9524cf47ab33d4c89607e7d1c4aa762084
Alexander Antoshkin <ighost99@gmail.com>
2024-03-03 23:13:00 +0300

    Initial commit

---
+++ gstackallocator/gstackallocator.hpp
@@ -0,0 +1,13 @@
+#ifndef GALG_GSTACKALLOCATOR_HPP
+#define GALG_GSTACKALLOCATOR_HPP
+
+namespace GAlg
+{
+    template <typename T>
+    class GStackAllocator
+    {
+        // TODO: Implement
+    };
+}
+
+#endif  // GALG_GSTACKALLOCATOR_HPP
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "gstackallocator/gstackallocator.hpp"
+
+// Using main() to run GAlg::GStackAllocator examples
+int main()
+{
+    GAlg::GStackAllocator<int> stackAlloc;
+
+    return 0;
+}
