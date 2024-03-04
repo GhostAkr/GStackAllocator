2bb53af501cb9e8e42ecf77ec2be0a1233cd20e7
Alexander Antoshkin <ighost99@gmail.com>
2024-03-04 23:53:53 +0300

    Implemented GStackAllocator::allocate()

--- gstackallocator/gstackallocator.hpp
+++ gstackallocator/gstackallocator.hpp
@@ -15,6 +15,29 @@ namespace GAlg
             , freeBlocks{{0, Size}}
         {}
 
+        T* allocate(size_t size)
+        {
+            // Find the first suitable free block
+            for (auto block : freeBlocks)
+            {
+                std::size_t blockStart = block.first;
+                std::size_t blockSize = block.second;
+
+                if (blockSize < size)
+                    continue;
+
+                // Suitable block found - update free blocks and return the pointer
+                freeBlocks.erase(blockStart);
+                if (size != blockSize)
+                    freeBlocks[blockStart + size] = blockSize - size;
+
+                return reinterpret_cast<T*>(memory.data() + blockStart);
+            }
+
+            // No free memory - throw an exception
+            throw std::bad_alloc();
+        }
+
     private:
         std::array<char, Size> memory;
 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
+#include <iostream>
+
 #include "gstackallocator/gstackallocator.hpp"
 
 // Using main() to run GAlg::GStackAllocator examples
 int main()
 {
-    GAlg::GStackAllocator<int, 1000000> stackAlloc;
+    GAlg::GStackAllocator<int, 20> stackAlloc;  // can fit 5 ints
+    
+    try
+    {
+        for (int i = 0; i < 5; ++i)
+        {
+            int* val = stackAlloc.allocate(sizeof(int));
+            *val = i;
+            std::cout << "Value " << *val << " at address " << val << std::endl;
+        }
+    }
+    catch (const std::bad_alloc& ex)
+    {
+        std::cout << "Bad alloc" << std::endl;
+    }
 
     return 0;
 }
